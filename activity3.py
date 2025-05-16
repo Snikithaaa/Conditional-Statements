ee3abc51276e11275ebe761176102a30acf67f2a
Snikithaaa <snima2021@gmail.com>
2025-05-16 19:39:57 +0530

    initial commit

---
+++ activity3.py
@@ -0,0 +1,11 @@
+num = int(input("Enter your number:"))
+if num > 15:
+    print("Your number is greater than 15" + "You're in the if block")
+
+else:
+    print("Your number is smaller than 15" + "You're in the else block")
+
+print("You're out of if and else block")
+
+
+    
