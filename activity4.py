7c585e0eccd750d4adc1eb783ca7029f81afe5c2
Snikithaaa <snima2021@gmail.com>
2025-05-16 19:50:41 +0530

    initial commit

---
+++ activity4.py
@@ -0,0 +1,13 @@
+num = int(input("Enter your number:"))
+if num%2==0:
+   print(f"Your number {num} is even")
+else:
+   print(f"Your number {num} is odd")
+
+num1 = int(input("ENter your number"))
+if (num1%10)>7:
+ print("Your number is greater than 7")
+else:
+  print("Your number is less than 7")
+
+
